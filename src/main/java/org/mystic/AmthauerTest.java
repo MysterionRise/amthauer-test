e24363fc0a227a3333ddc26ed8f4a67135bdac6d
Konstantin Perikov <pericov.ko@gmail.com>
2021-07-27 21:16:31 +0100

    add basic scene, gradle build, updated gitignore

---
+++ src/main/java/org/mystic/AmthauerTest.java
@@ -0,0 +1,15 @@
+package org.mystic;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class AmthauerTest extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("AmthauerTest");
+        primaryStage.show();
+    }
+
+
+}
