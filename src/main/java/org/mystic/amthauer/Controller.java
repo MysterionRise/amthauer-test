4bb516bd25ad9e5d800af542922a6a64cb3cf5c7
Konstantin Perikov <pericov.ko@gmail.com>
2021-08-07 13:43:43 +0100

    add in progress state, add ability to autotime tasks

--- src/main/java/org/mystic/amthauer/Controller.java
+++ src/main/java/org/mystic/amthauer/Controller.java
@@ -1,14 +1,15 @@
 package org.mystic.amthauer;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+import java.util.function.UnaryOperator;
 
 public class Controller {
 
@@ -23,6 +24,8 @@ public class Controller {
     @FXML
     Label testName;
     private String login;
+
+    private int currentStep = 0;
 
     @FXML
     private void closeWindow() {
@@ -54,16 +57,6 @@ public class Controller {
                     "Нажмите ДАЛЕЕ и начините работать, когда будете готовы. По истечении отведенного времени, форма выполнения заданий будет закрыта вне зависимости от того, успели Вы выполнить все задания или нет. Если Вы справитесь с заданиями быстрее, то в оставшееся время можно проверить свою работу или просто отдохнуть, но нельзя приступать к следующим заданиям. Переходить к следующим заданиям можно только по сигналу экспериментатора.\n");
             instructionText.setVisible(true);
             nextStep.setVisible(true);
-//            out = new PrintWriter((userName.ge
-//            tText() + (System.currentTimeMillis() % 100000)) + ".txt");
-//            this.login = userName.getText();
-//            out.println(login);
-//            final int[] steps = {1};
-//            startTestButton.setDisable(true);
-//            instructionText.setVisible(false);
-//            userName.setVisible(false);
-//            final List<ImageView> images = new ArrayList<>(9);
-//            goNextStep(steps, images);
         }
     }
 
@@ -72,14 +65,112 @@ public class Controller {
         userName.clear();
     }
 
+    Map<Integer, String> tasks = Map.ofEntries(
+            new SimpleEntry<>(1, "1. 1) писать 2) рубить З) ковать 4) шить 5) читать "),
+            new SimpleEntry<>(2, "2. 1) скоро 2) вскоре 3) в скором времени 4) завтра 5) сейчас  "),
+            new SimpleEntry<>(3, "3. 1) клиент 2) компаньон 3) подзащитный 4) покупатель 5) пациент"),
+            new SimpleEntry<>(4, "4. 1) существенный 2) примечательный 3) важный 4) характерный 5) типичный  ")
+    );
+
     @FXML
     public void nextStep() {
+        currentStep++;
+        switch (currentStep) {
+            case 1:
+                System.out.println("step 1");
+                tasks120();
+                break;
+            case 2:
+                System.out.println("step 2");
+                tasks2140();
+                break;
+            case 3:
+                System.out.println("step 3");
+                tasks2140();
+                break;
+        }
+
+    }
+
+    private void tasks2140() {
+        testName.setVisible(true);
+        testName.setText("ЗАДАНИЯ 21-40");
+        instructionText.setVisible(false);
+        Pane parent = (Pane) testName.getParent();
+        for (int i = 1; i <= 20; ++i) {
+            Label e = new Label(String.valueOf(i));
+            e.setPrefWidth(500);
+            e.setLayoutX(50);
+            e.setLayoutY(50 * i);
+            e.setText(tasks.getOrDefault(i, String.valueOf(i)));
+            parent.getChildren().add(e);
+            TextField inputField = new TextField("");
+            UnaryOperator<TextFormatter.Change> rejectChange = c -> {
+                if (c.isContentChange()) {
+                    for (char ch : c.getControlNewText().toCharArray()) {
+                        if (!Character.isDigit(ch)) {
+                            return null;
+                        }
+                    }
+                    return c;
+                }
+                return c;
+            };
+            inputField.setTextFormatter(new TextFormatter(rejectChange));
+            inputField.setId("task_" + i);
+            inputField.setPrefWidth(300);
+            inputField.setLayoutX(50);
+            inputField.setLayoutY(50 * i + 25);
+            parent.getChildren().add(inputField);
+            final Timeline animation = new Timeline(
+                    new KeyFrame(Duration.seconds(10),
+                            actionEvent -> {
+                                nextStep();
+                            }
+                    ));
+            animation.setCycleCount(1);
+            animation.play();
+        }
+    }
+
+    private void tasks120() {
         testName.setVisible(true);
         testName.setText("ЗАДАНИЯ 1-20");
         instructionText.setVisible(false);
         Pane parent = (Pane) testName.getParent();
-        for (int i = 0; i < 20; ++i) {
-           parent.getChildren().add(new Label(String.valueOf(i)));
+        for (int i = 1; i <= 20; ++i) {
+            Label e = new Label(String.valueOf(i));
+            e.setPrefWidth(500);
+            e.setLayoutX(50);
+            e.setLayoutY(50 * i);
+            e.setText(tasks.getOrDefault(i, String.valueOf(i)));
+            parent.getChildren().add(e);
+            TextField inputField = new TextField("");
+            UnaryOperator<TextFormatter.Change> rejectChange = c -> {
+                if (c.isContentChange()) {
+                    for (char ch : c.getControlNewText().toCharArray()) {
+                        if (!Character.isDigit(ch)) {
+                            return null;
+                        }
+                    }
+                    return c;
+                }
+                return c;
+            };
+            inputField.setTextFormatter(new TextFormatter(rejectChange));
+            inputField.setId("task_" + i);
+            inputField.setPrefWidth(300);
+            inputField.setLayoutX(50);
+            inputField.setLayoutY(50 * i + 25);
+            parent.getChildren().add(inputField);
+            final Timeline animation = new Timeline(
+                    new KeyFrame(Duration.seconds(5),
+                            actionEvent -> {
+                                nextStep();
+                            }
+                    ));
+            animation.setCycleCount(1);
+            animation.play();
         }
     }
 }
